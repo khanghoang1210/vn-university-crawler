7a695228efc1f24b973918dbe58b00b958878ec3
unknown <nguyenhoangkhang12102003@gmail.com>
2023-06-15 13:16:21 +0700

    data proccessing, write to the json file

--- university_crawler.py
+++ university_crawler.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-
+import json
 
 # Open the text file in read mode
 file_path = "./html.txt"
@@ -19,7 +19,17 @@ for row in rows:
     address = columns[2].text.strip()
 
     university["name"] = university_name
-    university["address"] = address
+    keyword = "Điện thoại" or "Tel"
+    start_index = address.find(keyword)
+    if start_index != -1:
+        university["address"] = address.replace(" –", ",")[9:start_index]
+    else:
+        university["address"] = address.replace(" –", ",")[9:]
     university_info.append(university)
 print(university_info)
 print(len(university_info))
+
+
+# Lưu danh sách vào tệp JSON
+with open('university.json', 'w',encoding='utf-8') as file:
+    json.dump(university_info, file, ensure_ascii=False, indent=4)
